d02449a0a842fef66fcad29864f90b9ca11cfb17
DryFear <voronin8945@mail.ru>
2019-09-29 15:51:23 +0300

    Fix test

--- app/src/test/java/ru/unfortunately/school/russianpassword/PasswordGeneratorTest.java
+++ app/src/test/java/ru/unfortunately/school/russianpassword/PasswordGeneratorTest.java
@@ -1,21 +1,15 @@
 package ru.unfortunately.school.russianpassword;
 
-import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Random;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class PasswordGeneratorTest {
 
     @Test
     public void generate() {
-        assertEquals(0, PasswordGenerator.generate(true, true, true, 0).length());
-        Random random = new Random();
         for (int i = 0; i < 30; i++) {
-            int len = random.nextInt(30);
-            assertEquals(len, PasswordGenerator.generate(true, true, true, len).length());
+            assertEquals(i, PasswordGenerator.generate(true, true, true, i).length());
         }
     }
 }
